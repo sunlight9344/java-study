45f969f83873bbd197da1b0726d28b4454f94646
sunlight9344 <sunlight9344@gmail.com>
2023-08-10 17:00:16 +0900

    0810/1700 TV doen

--- chapter03/src/main/java/tv/TV.java
+++ chapter03/src/main/java/tv/TV.java
@@ -6,15 +6,28 @@ public class TV {
 	private boolean power;  
 	
 	public TV(int channel, int volume, boolean power) {
-		
+		this.channel = channel;
+		this.volume = volume;
+		this.power = power;
 	}
 
 	public void power(boolean on) {
-		power = on;
+		this.power = on;
 	}
 	
 	public void channel(boolean up) {
+		if(up) {
+			this.channel += 1;	
+		}else{
+			this.channel -= 1;
+		}
 		
+		if(this.channel > 255) {
+			this.channel = 0;
+		}
+		if(this.channel < 0) {
+			this.channel = 255;
+		}
 	}
 	
 	public void channel(int channel) {
@@ -22,7 +35,17 @@ public class TV {
 	}
 	
 	public void volume(boolean up) {
-		
+		if(up) {
+			this.volume += 1;	
+		}else{
+			this.volume -= 1;
+		}
+		if(this.volume > 255) {
+			this.volume = 0;
+		}
+		if(this.volume < 0) {
+			this.volume = 100;
+		}
 	}
 	
 	public void volume(int volume) {
