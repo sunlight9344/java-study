4bfce16fbd7ecb1169dcfbf4756d6c6d88431c1c
sunlight9344 <sunlight9344@gmail.com>
2023-08-16 09:22:36 +0900

    0816/0922 CalendarTest

--- chapter04/src/main/java/chapter04/CalendarTest.java
+++ chapter04/src/main/java/chapter04/CalendarTest.java
@@ -7,6 +7,41 @@ public class CalendarTest {
 	public static void main(String[] args) {
 		Calendar cal = Calendar.getInstance();
 		
+		printDate(cal);
+		
+		cal.set(Calendar.YEAR, 2023);
+		cal.set(Calendar.MONTH, 11); // 12월
+		cal.set(Calendar.DATE, 25);
+		
+		printDate(cal);
+		
+		cal.set(1998,03,20,12,30,00);
+		cal.add(Calendar.DATE, 10000);
+		
+		printDate(cal);
+	}
+
+	private static void printDate(Calendar cal) {
+		
+		final String[] DAYS = {"일","월","화","수","목","금","토"};
+		
+		int year = cal.get(Calendar.YEAR);
+		int month = cal.get(Calendar.MONTH); // 0 ~ 11, +1
+		int date = cal.get(Calendar.DATE);
+		int day = cal.get(Calendar.DAY_OF_WEEK);
+		int hour = cal.get(Calendar.HOUR);
+		int minute = cal.get(Calendar.MINUTE);
+		int second = cal.get(Calendar.SECOND);
+		
+		System.out.println(
+				year + "/" +
+				(month + 1) + "/" +
+				date + " "+
+				DAYS[day-1] + "요일 "+
+				hour+":"+
+				minute +":"+
+				second
+				);
 		
 		
 	}
