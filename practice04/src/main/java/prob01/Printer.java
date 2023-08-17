d7fa75411372aea1551ecf25251154a71fbc4188
sunlight9344 <sunlight9344@gmail.com>
2023-08-17 09:41:00 +0900

    0817/0940 Generic Method

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -2,8 +2,27 @@ package prob01;
 
 public class Printer {
 
-	public void println(Object i) {
-		System.out.println(i);
+//	public void println(Object i) {
+//		System.out.println(i);
+//	}
+	
+	public <T> void println(T t) {
+		System.out.println(t);
 	}
-
+	
+	public int sum(Integer...nums) { 
+	int s = 0;
+	for(Integer i:nums) {
+		s += i;
+	}
+	return s;
+	}
+	
+	public <T> void println(T...ts) {
+		for(T t:ts) {
+			System.out.print(t);
+		}
+		System.out.println();
+	}
+	
 }
--- practice04/src/main/java/prob01/PrinterTest.java
+++ practice04/src/main/java/prob01/PrinterTest.java
@@ -4,10 +4,19 @@ public class PrinterTest {
 
 	public static void main(String[] args) {
 		Printer printer = new Printer();
-
 		printer.println( 10 );
 		printer.println( true );
 		printer.println( 5.7 );
 		printer.println( "홍길동" );
+		
+		System.out.println(printer.sum(1));
+		System.out.println(printer.sum(1,2));
+		System.out.println(printer.sum(1,2,3));
+		System.out.println(printer.sum(1,2,3,4));
+		System.out.println(printer.sum(1,2,3,4,5));
+		
+		printer.println(10,true);
+		printer.println("짜장",43);
+		printer.println(999,"ㅎㅎㅎㅎ");
 	}
 }
