8232a133a823514bbc86aad21c409aeeab637bba
sunlight9344 <sunlight9344@gmail.com>
2023-08-18 12:46:54 +0900

    0818/1246 Thread

--- network/src/main/java/echo/EchoServer.java
+++ network/src/main/java/echo/EchoServer.java
@@ -11,73 +11,35 @@ import java.net.Socket;
 import java.net.SocketException;
 
 public class EchoServer {
-
-	private static final int PORT = 8000;
+	public static final int PORT = 8000;
 	
 	public static void main(String[] args) {
-		
 		ServerSocket serverSocket = null;
 		
 		try {
 			serverSocket = new ServerSocket();
 			
-			serverSocket.bind(new InetSocketAddress("0.0.0.0",PORT),10);
-			log("starts...[port:"+PORT+"]");
+			serverSocket.bind(new InetSocketAddress("0.0.0.0", PORT), 10);
+			log("starts...[port:" + PORT + "]");
 			
-			Socket socket = serverSocket.accept();
-		
-			try {
-				InetSocketAddress remoteInetSocketAddress = (InetSocketAddress)socket.getRemoteSocketAddress();	
-				String remoteHostAddress = remoteInetSocketAddress.getAddress().getHostAddress();
-				int remotePort = remoteInetSocketAddress.getPort();
-				log("connected by client[" + remoteHostAddress + ":" + remotePort+"]");
-				
-
-				PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(),"utf-8"), true);
-				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(),"utf-8"));
-				
-				pw.print("방가루");
-				
-				while(true) {
-					String data = br.readLine();
-					if(data == null) {
-						log("closed by client");
-						break;
-					}
-					
-					log("received:" + data);
-					pw.println(data);
-				}
-				
-			} catch(SocketException e) {
-				log("suddenly closed by client");
-			}
-			catch(IOException e) {
-				log("error: " + e);
-			} finally {
-				try {
-					if(socket != null) {
-						socket.close();
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+			while(true) {
+				Socket socket = serverSocket.accept();
+				new Thread().run();
 			}
 		} catch (IOException e) {
-			System.out.println("errer:" + e);
+			log("error:" + e);
 		} finally {
 			try {
-				if(serverSocket != null) {
+				if(serverSocket != null && !serverSocket.isClosed()) {
 					serverSocket.close();
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-		}
+		}		
 	}
 	
 	private static void log(String message) {
 		System.out.println("[EchoServer] " + message);
 	}
-
 }
